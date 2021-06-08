f846b596bc13b76d8a9010ae2e2edf0a75312dab
rafaelhbastos <rafaelhenribastos12@gmail.com>
2021-06-08 11:28:52 -0300

    Inserting keydown Enter event

--- script/modules/get-search.js
+++ script/modules/get-search.js
@@ -5,8 +5,19 @@ export default function getSearch () {
     const input = document.querySelector('.input');
     
     btn.addEventListener('click', callFetch);
+    window.addEventListener('keydown', enterCallFetch);
 
     function callFetch () {
-        fetchUser(`https://api.github.com/users/${input.value}`);
+        const user = input.value;
+        input.value = '';
+        fetchUser(`https://api.github.com/users/${user}`);
+    }
+
+    function enterCallFetch(event) {
+        if (event.key === 'Enter') {
+            const user = input.value;
+            input.value = '';
+            fetchUser(`https://api.github.com/users/${user}`);
+        }
     }
 }
