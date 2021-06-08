c365c056339fb265669c3f8fdd9df8272a4d8ce2
rafaelhbastos <rafaelhenribastos12@gmail.com>
2021-06-07 21:23:04 -0300

    Fixing the loading of the search image

--- script/modules/end-loading.js
+++ script/modules/end-loading.js
@@ -3,13 +3,6 @@ export default function endLoading() {
     const loading = document.querySelector('.loader');
     const loadingText = document.querySelector('.loader-text');
 
-    const list = document.querySelector('.list');
-    
-    if(list){
-        list.classList.add('active');
-    }
-
-
     container.removeChild(loading);
     container.removeChild(loadingText);
 
